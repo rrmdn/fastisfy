a8d772ff433bd3d4795c94486b8af48ed9968633
Rizki Romadhoni <rrmdn@pm.me>
2022-12-27 08:57:04 +0700

    update test

---
+++ packages/fastisfy/src/commands/dev.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from "@oclif/test";
+import axios from "axios";
+
+describe("dev", () => {
+  test
+    .stdout()
+    .command(["dev"])
+    .it("runs dev cmd", async (ctx) => {
+      expect(ctx.stdout).to.contain("Development server started");
+      const resp = await axios.get(
+        "http://localhost:3000/v1/products/123/reviews"
+      );
+      expect(resp.data).to.deep.equal({
+        data: [
+          {
+            comment: "Great product",
+            created_at: "2021-01-01",
+            id: "1",
+            product_id: "123",
+            rating: 4,
+            updated_at: "2021-01-01",
+            user_id: "",
+          },
+        ],
+        meta: {
+          total: 1,
+        },
+      });
+    });
+});
